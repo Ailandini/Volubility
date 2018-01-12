028892a9c326addb09e915973521995aaef56a61
Landini <landini@uiowa.edu>
2018-01-12 12:27:01 -0600

    Update Mainfile

--- MainForm.cs
+++ MainForm.cs
@@ -10,6 +10,9 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using System.Linq;
 using System.Media;
 
 
@@ -42,21 +45,89 @@ namespace Volubility
 				sSelectedPath = ofd.FileName;
 				selection_text.Text = sSelectedPath;
 				submit_button.Enabled = true;
+				
+				byte[] ba = File.ReadAllBytes(sSelectedPath);
+				string outp = ba[0].ToString();
+				for(int i=1; i< ba.Length; i++){
+					outp += "|";
+					outp += ba[i].ToString();
+					if(i == 3)
+					{
+						outp+= "\t\tRIFF\n";
+					}
+					if(i == 7)
+					{
+						outp+= "\tFile Size\n";
+					}
+					if(i == 14)
+					{
+						outp+= "\tfmt \n";
+					}
+					if(i == 19)
+					{
+						outp+= "\tfmt Header Length\n";
+					}
+					if(i == 21)
+					{
+						outp+= "\t\t\tFormat Type\n";
+					}
+					if(i == 23)
+					{
+						outp+= "\t\t\tChannels\n";
+					}
+					if(i == 27)
+					{
+						outp+= "\t\tSample Rate\n";
+					}
+					if(i == 31)
+					{
+						outp+= "\t\tByte Rate\n";
+					}
+					if(i == 33)
+					{
+						outp+= "\t\t\tBlock Align\n";
+					}
+					if(i == 39)
+					{
+						outp+= "\tData\n";
+					}
+					if(i == 43)
+					{
+						outp+= "\t\tChunk Size\n";
+					}
+						
+					
+					if(i == 11)
+					{
+						outp += "\tWAVE\n\n";
+					}
+					if(i == 35)
+					{
+						outp += "\t\t\tBits Per Sample\n\n";	
+					}
+					
+				}
+				
+				File.WriteAllText("Bytes Organized", outp);
 			}
+			
 		}
 		private int position;
 		private int count;
 		byte[] byteArray;
 		
-		void ParseAudio(){
-			byte[] ba;
+		void ParseAudio(string sSelectedPath){
+			FileStream fs = File.Open(sSelectedPath, FileMode.Open, FileAccess.Read);
+			byteArray = File.ReadAllBytes(sSelectedPath);
+			fs.Close();
+			Console.WriteLine(Encoding.Default.GetString(byteArray));
 		}
 		
 		void Submit_buttonClick(object sender, EventArgs e)
 		{
 			SoundPlayer song = new SoundPlayer(sSelectedPath);
 			song.Play();
-			ParseAudio();
+			//ParseAudio(sSelectedPath);
 		}
 	}
 }
